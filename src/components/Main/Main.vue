44b5b6fa3eaf23e5e553e679f8062382f96c9e26
Zhao KK <kikakkz@hotmail.com>
2022-10-03 15:36:28 +0800

    Add initialize page

---
+++ src/components/Main/Main.vue
@@ -0,0 +1,45 @@
+<template>
+  <div view='lHh Lpr lFf'>
+    <q-img :src='logo' style='width: 600px' fit='contain' />
+    <q-input
+      v-model="search"
+      debounce="500"
+      rounded
+      outlined
+      disable
+      placeholder='Coming soon'
+    >
+      <template v-slot:append>
+        <q-icon name="search" />
+      </template>
+    </q-input>
+    <div class='row looking'>
+      <q-space />
+      <q-btn label='Just looking' rounded flat>
+        <q-tooltip>
+          Coming soon
+        </q-tooltip>
+      </q-btn>
+      <q-space />
+    </div>
+    <div class='occupier' />
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { ref } from 'vue'
+
+import logo from '../../assets/logo/logo.png'
+
+const search = ref('')
+
+</script>
+
+<style scoped lang='sass'>
+.looking
+  margin: 10px 0 10px 0
+  color: $grey-8
+
+.occupier
+  height: 200px
+</style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,53 @@
+<template>
+  <q-layout view='lHh Lpr lFf'>
+    <q-header elevated>
+      <q-toolbar>
+        <q-space />
+        <a class='tools' href='#/whitepaper'>White Paper</a>
+        <a class='tools' href='#/deck'>Deck</a>
+        <a class='tools' href='#/blog'>Blog</a>
+        <q-btn avatar :icon='"img:" + metamask' flat dense round size='18px'>
+          <q-tooltip>
+            Coming soon
+          </q-tooltip>
+        </q-btn>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated>
+      <q-toolbar>
+        <div class='footer'>© 2022 - Cyber Tracer</div>
+      </q-toolbar>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script setup lang='ts'>
+
+import metamask from '../assets/icon/metamask.webp'
+
+</script>
+
+<style scoped lang='sass'>
+.q-layout__section--marginal
+  background-color: transparent
+  color: $grey-9
+  font-size: 16px
+
+.logo
+  height: 96px
+  width: 120px
+
+.tools
+  margin: 0 10px 0 10px
+  text-decoration: none
+  color: $grey-9
+
+.footer
+  color: $blue-14
+  font-size: 14px
+</style>
