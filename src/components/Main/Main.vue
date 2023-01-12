9ecfb0269484d2c16e980d8aa31c535c7da41b97
lidaqiang <dqli161@163.com>
2023-01-12 14:59:33 +0800

    support image drag & upload btn

--- src/components/Main/Main.vue
+++ src/components/Main/Main.vue
@@ -9,12 +9,11 @@
       v-model="search"
       rounded
       outlined
-      disable
       style='width: 650px;'
       placeholder='Coming soon'
     >
       <template v-slot:append>
-        <q-icon name="search" />
+        <q-icon name='camera_enhance' @click='photographer = true' />
       </template>
     </q-input>
     <div class='row looking'>
@@ -25,9 +24,52 @@
         </q-tooltip>
       </q-btn>
       <q-space />
+
     </div>
     <div class='occupier' />
   </div>
+  <q-dialog v-model='photographer'>
+      <q-card style="width: 700px; max-width: 80vw; height: 260px; max-height: 260px;">
+        <q-uploader
+          url="http://localhost:4444/upload"
+          label="Custom header"
+          multiple
+          color='white'
+          text-color='black'
+          class='upload-box'
+        >
+      <template v-slot:header="scope">
+        <div class="row no-wrap items-center q-pa-sm q-gutter-xs">
+          <q-btn v-if="scope.queuedFiles.length > 0" icon="clear_all" @click="scope.removeQueuedFiles" round dense flat >
+            <q-tooltip>Clear All</q-tooltip>
+          </q-btn>
+          <q-btn v-if="scope.uploadedFiles.length > 0" icon="done_all" @click="scope.removeUploadedFiles" round dense flat >
+            <q-tooltip>Remove Uploaded Files</q-tooltip>
+          </q-btn>
+          <q-spinner v-if="scope.isUploading" class="q-uploader__spinner" />
+          <!-- <div class="col">
+            <div class="q-uploader__title">Upload your files</div>
+            <div class="q-uploader__subtitle">{{ scope.uploadSizeLabel }} / {{ scope.uploadProgressLabel }}</div>
+          </div> -->
+          <div>
+            <span>Drag an image here or</span>
+            <q-btn v-if="scope.canAddFiles" flat color="primary" @click="scope.pickFiles">
+              upload a file
+              <q-uploader-add-trigger />
+            </q-btn>
+          </div>
+          <q-btn v-if="scope.canUpload" icon="cloud_upload" @click="scope.upload" round dense flat >
+            <q-tooltip>Upload Files</q-tooltip>
+          </q-btn>
+
+          <q-btn v-if="scope.isUploading" icon="clear" @click="scope.abort" round dense flat >
+            <q-tooltip>Abort Upload</q-tooltip>
+          </q-btn>
+        </div>
+      </template>
+    </q-uploader>
+      </q-card>
+    </q-dialog>
 </template>
 
 <script setup lang='ts'>
@@ -36,6 +78,8 @@ import { ref } from 'vue'
 import logo from '../../assets/logo/logo.png'
 
 const search = ref('')
+
+const photographer = ref(false)
 
 </script>
 
@@ -49,4 +93,9 @@ const search = ref('')
 
 .occupier
   height: 240px
+
+.upload-box
+  width: 100%
+  v::deep q-uploader_header
+    color: lightwhite
 </style>
