e84136dbc6afd194e72fbd4cbeed806679cdf0b0
daqiangli <dqli161@163.com>
2023-05-15 14:39:35 +0800

    support drag & input

--- src/components/Main/Main.vue
+++ src/components/Main/Main.vue
@@ -6,89 +6,48 @@
       <q-space />
     </div>
     <q-input
-      v-model="search"
+      v-if='isText'
+      class='input-container'
       rounded
       outlined
-      style='width: 650px;'
-      placeholder='Coming soon'
+      v-model="search"
+      placeholder="input text here" 
     >
       <template v-slot:append>
-        <q-icon name='camera_enhance' @click='photographer = true' />
+        <InputOption v-model:option='option' />
       </template>
     </q-input>
-    <div class='row looking'>
-      <q-space />
-      <q-btn label='Just looking' rounded flat>
-        <q-tooltip>
-          Coming soon
-        </q-tooltip>
-      </q-btn>
-      <q-space />
-
-    </div>
+    <q-file
+      v-if='!isText'
+      class='input-container'
+      v-model="filesPng"
+      rounded
+      outlined
+      label="drag a image here"
+      multiple
+    >
+      <template v-slot:append>
+        <InputOption v-model:option='option' />
+      </template>
+    </q-file>
     <div class='occupier' />
   </div>
-  <q-dialog v-model='photographer'>
-      <q-card style="width: 700px; max-width: 80vw; height: 300px; max-height: 300px;">
-        <q-uploader
-          label="Custom header"
-          single
-          color='white'
-          text-color='black'
-          class='upload-box'
-          field-name='upload'
-          :form-fields='[{name: "topN", value: "10"}]'
-          auto-upload
-          @failed='onFailed'
-          @uploaded='onUploaded'
-          :factory='factoryFn'
-        >
-      <template v-slot:header="scope">
-        <div class="row no-wrap items-center q-pa-sm q-gutter-xs">
-          <q-btn v-if="scope.queuedFiles.length > 0" icon="clear_all" @click="scope.removeQueuedFiles" round dense flat >
-            <q-tooltip>Clear All</q-tooltip>
-          </q-btn>
-          <q-btn v-if="scope.uploadedFiles.length > 0" icon="done_all" @click="scope.removeUploadedFiles" round dense flat >
-            <q-tooltip>Remove Uploaded Files</q-tooltip>
-          </q-btn>
-          <q-spinner v-if="scope.isUploading" class="q-uploader__spinner" />
-          <!-- <div class="col">
-            <div class="q-uploader__title">Upload your files</div>
-            <div class="q-uploader__subtitle">{{ scope.uploadSizeLabel }} / {{ scope.uploadProgressLabel }}</div>
-          </div> -->
-          <div>
-            <span>Drag an image here or</span>
-            <q-btn flat color="primary" @click="scope.pickFiles">
-              upload a file
-              <q-uploader-add-trigger />
-            </q-btn>
-          </div>
-          <!-- <q-btn v-if="scope.canUpload" icon="cloud_upload" @click="scope.upload" round dense flat >
-            <q-tooltip>Upload Files</q-tooltip>
-          </q-btn> -->
-
-          <q-btn v-if="scope.isUploading" icon="clear" @click="scope.abort" round dense flat >
-            <q-tooltip>Abort Upload</q-tooltip>
-          </q-btn>
-        </div>
-      </template>
-    </q-uploader>
-      </q-card>
-    </q-dialog>
 </template>
 
 <script setup lang='ts'>
-import { read } from 'fs';
 import { useNFTMetaStore } from 'src/localstore/nft';
 import { UploadResponse } from 'src/localstore/nft/types';
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { useRouter } from 'vue-router';
-
+import InputOption from 'src/components/Main/InputOption.vue'
 import logo from '../../assets/logo/logo.png'
 
 const search = ref('')
-const photographer = ref(false)
 
+const option = ref('File')
+const isText = computed(() => option.value === 'Text')
+
+const filesPng = ref(null)
 const router = useRouter()
 
 const nft = useNFTMetaStore()
@@ -97,6 +56,7 @@ const onUploaded = (info: {
     /**
      * Uploaded files
      */
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     files: readonly any[];
     /**
      * XMLHttpRequest that has been used to upload this batch of files
@@ -134,6 +94,8 @@ const onFailed = () => {
 .logo
   margin: 10px 0 20px 0
 
+.input-container
+  width: 650px
 .looking
   margin: 10px 0 10px 0
   color: $grey-8
