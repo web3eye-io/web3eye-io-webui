57f8531bbd308c81211616ee7650407bb31c6426
daqiangli <dqli161@163.com>
2023-05-24 15:31:59 +0800

    use new q-uploader

--- src/components/Main/Main.vue
+++ src/components/Main/Main.vue
@@ -1,5 +1,5 @@
 <template>
-  <div view='lHh Lpr lFf'>
+  <div view='lHh Lpr lFf search-container'>
     <div class='row logo'>
       <q-space />
       <q-img :src='logo' style='width: 400px' fit='contain' />
@@ -15,7 +15,7 @@
       placeholder="input text here"
     >
       <template v-slot:append>
-        <InputOption v-model:option='curOption' />
+        <InputOption v-model:option='curOption' :disable='uploading' />
       </template>
     </q-input>
     <q-uploader
@@ -30,6 +30,8 @@
       flat
       @failed='onFailed'
       @uploaded='onUploaded'
+      @added='onAdded'
+      @uploading='uploading = true'
     >
       <template v-slot:header>
           <q-input
@@ -37,10 +39,11 @@
             rounded
             outlined
             v-model="fileName"
+            :loading="uploading"
           >
           <q-uploader-add-trigger />
           <template v-slot:append>
-            <InputOption v-model:option='curOption' />
+            <InputOption v-model:option='curOption' :disable='uploading' />
           </template>
         </q-input>
       </template>
@@ -55,89 +58,37 @@
 import { useNFTMetaStore } from 'src/localstore/nft';
 import { UploadResponse } from 'src/localstore/nft/types';
 import { computed, ref } from 'vue'
-// import { useRouter } from 'vue-router';
 import InputOption from 'src/components/Main/InputOption.vue'
 import logo from '../../assets/logo/logo.png'
 import { useRouter } from 'vue-router'
-// import { api } from 'src/boot/axios';
 
 const curOption = ref('File')
 const isText = computed(() => curOption.value === 'Text')
 
 const search = ref('')
-const fileName = ref('')
-// const file = ref({} as File)
-
-// const router = useRouter()
-
-// const nft = useNFTMetaStore()
-
-// const uploading = ref(false)
-// Contract search
 const handleEnter = () => {
   console.log('enter......')
 }
 
-// // image search
-// const onUpdate  = (file: File) => {
-//   // const imageUrl = URL.createObjectURL(file);
-//   // console.log('url: ', imageUrl)
-//   // console.log('file: ', file)
-//   uploading.value = true
-//   const formData = new FormData()
-//   formData.append('topN', '10')
-//   formData.append('upload',  new Blob([file], {type: 'image/*'}))
-//   api.post('/api/entrance/search/file', formData, { headers: {'Content-Type': 'multipart/form-data'},})
-//   .then((response) => {
-//     console.log('response: ', response.data)
-//     nft.setNftMeta(response.data as Array<NFTMeta>)
-//     void router.push({
-//       path: '/result'
-//     })
-//   })
-//   .catch((error)=> {
-//     console.log('error: ', error)
-//   })
-//   .finally(() => {
-//     uploading.value = false
-//   })
+const fileName = ref('')
+const uploading = ref(false)
 
-//   const reader = new FileReader()
-//   // reader.readAsArrayBuffer(file)
-//   reader.onload = function () {
-//     // console.log('result', reader.result)
-    
-//   }
-// }
+const onAdded = (files: readonly any[]) => {
+  const _file = files[0] as File
+  fileName.value = _file.name
+}
 
-// const handleReject = () => {
-//   console.log('handleReject')
-//   uploading.value = false
-// }
-
-// const handleClear = () => {
-//   console.log('clear')
-//   file.value = {} as File
-// }
 
 const router = useRouter()
 
 const nft = useNFTMetaStore()
 const onUploaded = (info: {
-    /**
-     * Uploaded files
-     */
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     files: readonly any[];
-    /**
-     * XMLHttpRequest that has been used to upload this batch of files
-     */
     xhr: XMLHttpRequest;
   }) => {
   const reader = new FileReader()
   reader.readAsDataURL(info.files[0] as Blob)
   reader.onload = function() { 
-    // console.log('result: ', this.result) // binary
     nft.NTFMetas.Current = window.URL.createObjectURL(info.files[0] as Blob)
 	}
   const response = JSON.parse(info.xhr.response as string) as UploadResponse
@@ -145,9 +96,11 @@ const onUploaded = (info: {
   void router.push({
     path: '/result'
   })
+  uploading.value = false
 }
 
 const onFailed = () => {
+  uploading.value = false
   console.log('onFailed...')
 }
 
@@ -167,9 +120,10 @@ const onFailed = () => {
   height: 240px
 
 .upload-box
-  width: 100%
-  height: 300px
-  max-height: 300px
-  // ::v-deep .q-uploader__list 
-  //   display: hidden
+  width: 650px
+  flex-direction: row
+.search-container
+  ::v-deep > div.q-uploader
+    width: auto
+    max-height: 160px
 </style>
