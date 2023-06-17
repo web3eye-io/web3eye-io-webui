b9e96aacef056b9f0f4f0559c6d8340f92f236d6
daqiangli <dqli161@163.com>
2023-06-17 11:55:04 +0800

    add type definition

---
+++ src/teststore/snapshot/types.ts
@@ -0,0 +1,40 @@
+export interface ContentItem {
+  ID: string;
+  URI: string;
+  ChainType: string;
+  ChainID: string;
+  Contract: string;
+  TokenID: string;
+  FileName: string;
+}
+
+enum BackupState {
+  DefaultBackupState  = 'DefaultBackupState',
+  BackupStateNone     = 'BackupStateNone',
+  BackupStateCreated  = 'BackupStateCreated',
+  BackupStateProposed = 'BackupStateProposed',
+  BackupStateAccepted = 'BackupStateAccepted',
+  BackupStateSuccess  = 'BackupStateSuccess',
+  BackupStateFail     = 'BackupStateFail',
+}
+
+export interface Snapshot {
+  ID: string;
+  Index: number;
+  SnapshotCommP: string;
+  SnapshotRoot: string;
+  SnapshotURI: string;
+  Items: ContentItem[];
+  BackupState: BackupState;
+  ProposalCID: string;
+  DealID: number;
+}
+
+export interface GetSnapshotsRequest {
+  Indexes: number[];
+}
+
+export interface GetSnapshotsResponse {
+  Infos: Snapshot[];
+  Total: number;
+}
